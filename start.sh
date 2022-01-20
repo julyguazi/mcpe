a743966f5593f0d7f23d1b1886e710ed27f7e4e5
瓜子 <89156163+julyguazi@users.noreply.github.com>
2022-01-20 16:00:38 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 screen_name="mc"
 screen -dmS $screen_name
-cmd="cd ~/mc"
+cmd="cd /home/ubuntu/mc"
 screen -x -S $screen_name -p 0 -X stuff "$cmd"
 screen -x -S $screen_name -p 0 -X stuff '\n'
 screen -x -S $screen_name -p 0 -X stuff "LD_PRELOAD= ./bedrock_server"
